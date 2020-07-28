fab093db4630f68dfd7b108002aea5499e0bb640
Damiansalguero <damian.salguero@aikq.de>
2020-07-28 16:04:27 +0200

    add RollDice logic

--- dice_app/src/Die.js
+++ dice_app/src/Die.js
@@ -2,11 +2,7 @@ import React, { Component } from "react";
 import "./Die.css";
 class Die extends Component {
   render() {
-    return (
-      <div>
-        <i className={`Die fas fa-dice-${this.props.face}`} />
-      </div>
-    );
+    return <i className={`Die fas fa-dice-${this.props.face}`} />;
   }
 }
 export default Die;
