316a77bfe019e4e78b2a4b26292b0c275c8cf408
Damiansalguero <damian.salguero@aikq.de>
2020-07-28 16:34:06 +0200

    add final css & animations

--- dice_app/src/Die.js
+++ dice_app/src/Die.js
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 import "./Die.css";
 class Die extends Component {
   render() {
-    return <i className={`Die fas fa-dice-${this.props.face}`} />;
+    return (
+      <i
+        className={`Die fas fa-dice-${this.props.face} ${this.props.rolling &&
+          "shaking"}`}
+      />
+    );
   }
 }
 export default Die;
