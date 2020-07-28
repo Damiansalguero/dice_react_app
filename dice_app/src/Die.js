edeb961859f66eba37c45b9275920387e6aa151b
Damiansalguero <damian.salguero@aikq.de>
2020-07-28 15:40:24 +0200

    add compnents, update css

---
+++ dice_app/src/Die.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Die.css";
+class Die extends Component {
+  render() {
+    return (
+      <div>
+        <i className={`Die fas fa-dice-${this.props.face}`} />
+      </div>
+    );
+  }
+}
+export default Die;
